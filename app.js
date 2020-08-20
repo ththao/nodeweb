765863191beaa36e71d309ace1dbf5082639b607
Thao Tran <ththao@ceresolutions.com>
2020-08-20 09:52:51 +0700

    update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const port = 3000;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/user');
@@ -23,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use('/department', departmentRouter);
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
